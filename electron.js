737b4427efa766fa0ca07398828f44952c814ecd
ianikian <tikomayorista@gmail.com>
2025-08-09 19:56:50 -0300

    Add electron.js 1754780211001

---
+++ electron.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  })
+
+  // Load the Next.js app running on localhost:8000
+  win.loadURL('http://localhost:8000')
+
+  // Open DevTools optionally
+  // win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
